ba8e42e56fa46b2d4f3f3594b2885b74b22a65fc
EYK02 <60719559+EYK02@users.noreply.github.com>
2026-06-14 16:58:13 +0200

    Implement FGSM attack, accuracy drops from 98.68% ti 63.3% at epsilon 0.30

---
+++ attacks/fgsm.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+def fgsm_attack(model, device, data, target, epsilon):
+    data.requires_grad = True
+    output = model(data)
+    loss = nn.CrossEntropyLoss()(output, target)
+    model.zero_grad()
+    loss.backward()
+    data_grad = data.grad.data
+    perturbed_data = data + epsilon * data_grad.sign()
+    perturbed_data = torch.clamp(perturbed_data, -1, 1)
+    return perturbed_data
