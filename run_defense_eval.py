bd6e5981736ce27b5b6c9c734ebc6bea38787fbc
EYK02 <60719559+EYK02@users.noreply.github.com>
2026-06-16 14:44:51 +0200

    Update run defense_eval on strongest attack for all defenses for seed=0. Based on results, run other combinations

--- run_defense_eval.py
+++ run_defense_eval.py
@@ -1,29 +1,73 @@
 # run_defense_eval.py
 
 import sys
+
 from utils.runner import Experiment, ExperimentRunner
 
 runner = ExperimentRunner()
 
 experiments = []
 
-for seed in range(5):
-    experiments.append(
-        Experiment(
-            f"FGSM defense seed {seed}",
-            [
+seeds = range(1)
+
+defenses = [
+    ("fgsm", None),
+    ("pgd", 10),
+    ("pgd", 20),
+    ("pgd", 5),
+    ("pgd", 40),
+]
+
+eval_attacks = [
+    ("pgd", 40),
+]
+
+for seed in seeds:
+    for defense_attack, defense_steps in defenses:
+        for eval_attack, eval_steps in eval_attacks:
+
+            cmd = [
                 sys.executable,
                 "-m",
                 "defenses.evaluate_defense",
-                "--attack",
-                "fgsm",
-                "--defense",
-                "fgsm",
+
+                "--defense_attack",
+                defense_attack,
+
+                "--defense_epsilon",
+                "0.2",
+
+                "--eval_attack",
+                eval_attack,
+
                 "--seed",
-                str(seed)
+                str(seed),
             ]
-        )
-    )
+
+            if defense_steps is not None:
+                cmd.extend([
+                    "--defense_steps",
+                    str(defense_steps),
+                ])
+
+            if eval_steps is not None:
+                cmd.extend([
+                    "--eval_steps",
+                    str(eval_steps),
+                ])
+
+            experiments.append(
+                Experiment(
+                    (
+                        f"def={defense_attack}"
+                        f"{defense_steps or ''} "
+                        f"atk={eval_attack}"
+                        f"{eval_steps or ''} "
+                        f"seed={seed}"
+                    ),
+                    cmd,
+                )
+            )
 
 for exp in experiments:
     runner.run(exp)
