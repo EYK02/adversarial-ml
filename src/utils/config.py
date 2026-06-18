5358880d7e7d0fa13af0f61a64bd887813570ecd
EYK02 <60719559+EYK02@users.noreply.github.com>
2026-06-18 20:02:23 +0200

    Update config comment regular for dry run

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,13 +1,14 @@
 # src/utils/config.py
 
-EPSILONS = [0.1, 0.2, 0.3] # 0.0-0.3 step 0.05
+#EPSILONS = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3] 
+EPSILONS =  [0.1, 0.2, 0.3] # dry
 BATCH_SIZE = 64
 
 LEARNING_RATE = 0.001
 NUM_EPOCHS = 5
-NUM_SEEDS = 1 # 5
+#NUM_SEEDS = 5 
+NUM_SEEDS = 1 # dry
 
-DEFENSES = [("fgsm", None), ("pgd", 10)] 
 
 # DEFENSES = [
 #     ("fgsm", None),
@@ -16,7 +17,8 @@ DEFENSES = [("fgsm", None), ("pgd", 10)]
 #     ("pgd",  40),
 # ]
 
-EVAL_ATTACKS = [
-    ("fgsm", None),
-    ("pgd", 10) #change back to 40
-]
+DEFENSES = [("fgsm", None), ("pgd", 10)]  #dry run
+
+#EVAL_ATTACKS = [("fgsm", None), ("pgd", 40)]
+
+EVAL_ATTACKS = [("fgsm", None), ("pgd", 41)] #dry
